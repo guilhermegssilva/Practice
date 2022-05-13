308322722c7f16f14771a66c917ae3ca1fe0d80e
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-05-13 18:42:30 -0300

    Add files via upload

---
+++ rocket-cofee/main.js
@@ -0,0 +1,18 @@
+const navMenu = document.getElementById('menu-mobile');
+
+function toggleMenu(){
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+//remover ao clicar em alguma opção do menu mobile
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('active');
+  })
+}
+
+
+navMenu.addEventListener('click', toggleMenu);
