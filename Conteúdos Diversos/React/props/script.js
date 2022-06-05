a6e8ad64c753d1d0d5979c99266067c276d7b542
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-06-05 12:56:59 -0300

    Add files via upload

---
+++ Conteúdos Diversos/React/props/script.js
@@ -0,0 +1,52 @@
+import React from "react"
+import Contact from "./Contact"
+
+export default function App() {
+    return (
+        <div className="contacts">
+            <Contact 
+                img="./images/mr-whiskerson.png" 
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact 
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
+        </div>
+    )
+}
+
+
+export default function Contact(props) { //O atributo props ficam no componente em que será criado as tags que serão posteriormente adicionadas em outro componente
+    
+    return (  //As informações são substituídas por props.nome-da-propriedade
+        <div className="contact-card">
+            <img src={props.img}/>
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{props.email}</p>
+            </div>
+        </div>
+    )
+}
