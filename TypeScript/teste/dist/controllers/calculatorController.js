93ba34f48cffc3afbcea86efee8f0f6f3bbc53e8
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2023-01-19 06:47:25 -0300

    Delete TypeScript/teste directory

--- TypeScript/teste/dist/controllers/calculatorController.js
+++
@@ -1,8 +0,0 @@
-function convertCLTToPJ(req, res) {
-    var salary = req.query.salary;
-    var newSalary = Number(salary) + (Number(salary) * 0.3);
-    res.send({
-        resultado: "O seu sal\u00E1rio deve ser pelo menos: ".concat(newSalary)
-    });
-}
-export { convertCLTToPJ, };
